458d1f97d88c598547ed2515a71986d7d54b1d9b
Jeremiah Kellick <jeremiahkellick@icloud.com>
2017-05-03 19:17:14 +0000

    add navbar with wordpress dynamics

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ function bfg_setup_assets() {
 
   wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/compiled.css');
 
+  wp_register_script( 'jQuery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', null, null, true );
+  wp_enqueue_script( 'jQuery' );
   wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', null, null, true);
 
 }
