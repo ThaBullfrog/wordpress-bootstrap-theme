ebe9a77d6ac6b1a5f84295fb2dffa3f5d3e88c3e
thabullfrog <th3bullfrog@gmail.com>
2017-05-03 16:45:28 -0400

    add color

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
 
 if(have_posts()):
   while(have_posts()): the_post(); ?>
-    <h3><?php the_title(); ?></h3>
-    <p><?php the_content(); ?></p>
+    <?php the_content(); ?>
   <?php endwhile;
 endif;
 
